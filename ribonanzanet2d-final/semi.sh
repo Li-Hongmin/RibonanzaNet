0fedcee969d6e6e6294fa388d40b014dc4564fdd
HONGMIN LI <lihongmin@alumni.u-tokyo.ac.jp>
2024-07-23 08:08:00 +0900

    semi

---
+++ ribonanzanet2d-final/semi.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#------ pjsub option --------# 
+#PJM -L rscgrp=share
+#PJM -L gpu=1
+#PJM -L elapse=48:00:00 
+#PJM -g gs58
+#PJM -j
+
+
+#------- Program execution -------#
+export MPLCONFIGDIR="/work/gs58/d58004/tmp/matplotlib"
+export WANDB_CONFIG_DIR="/work/gs58/d58004/tmp/wandb"
+export PATH="/work/02/gs58/d58004/mambaforge/envs/torch/bin/:$PATH"
+nvidia-smi
+cd /work/gs58/d58004/ideas/RibonanzaNet/ribonanzanet2d-final
+echo "Start training"
+# echo "Step1...................."
+# python step1.py
+echo “Step2....................”
+#python step2.py
+echo “Step3....................”
+python step3_68_pseudo_recompute.py
+echo "make_submission...................."
+python make_submission.py --para RibonanzaNet-Deg_30_68_re.pt
+python make_submission.py --para RibonanzaNet-Deg_31_68_re.pt
