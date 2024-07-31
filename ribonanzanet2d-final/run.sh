09fd34f66bc9c735853f2a11bc1bf1a8d37a517e
HONGMIN LI <lihongmin@alumni.u-tokyo.ac.jp>
2024-07-31 20:28:25 +0900

    del tab

--- ribonanzanet2d-final/run.sh
+++ ribonanzanet2d-final/run.sh
@@ -9,10 +9,10 @@
 
 
 #------- Program execution -------#
-    export MPLCONFIGDIR="/work/gs58/d58004/tmp/matplotlib"
-    export WANDB_CONFIG_DIR="/work/gs58/d58004/tmp/wandb"
-    export PATH="/work/02/gs58/d58004/mambaforge/envs/torch/bin/:$PATH"
-    nvidia-smi
-    cd /work/gs58/d58004/ideas/RibonanzaNet/ribonanzanet2d-final
-    echo "Start training"
-    python train_ribonanzanet.py --save_dir saved_models_mamaba
+export MPLCONFIGDIR="/work/gs58/d58004/tmp/matplotlib"
+export WANDB_CONFIG_DIR="/work/gs58/d58004/tmp/wandb"
+export PATH="/work/02/gs58/d58004/mambaforge/envs/torch/bin/:$PATH"
+nvidia-smi
+cd /work/gs58/d58004/ideas/RibonanzaNet/ribonanzanet2d-final
+echo "Start training"
+python train_ribonanzanet.py --save_dir saved_models_mamaba
