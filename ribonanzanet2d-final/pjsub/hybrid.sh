e621d90e0b267ca7da73b7e1c7ece204d85b0060
HONGMIN LI <lihongmin@alumni.u-tokyo.ac.jp>
2024-08-13 07:26:32 +0900

    hybrid

---
+++ ribonanzanet2d-final/pjsub/hybrid.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#------ pjsub option --------# 
+#PJM -L rscgrp=share
+#PJM -L gpu=1
+#PJM -L elapse=48:00:00 
+#PJM -g gs58
+#PJM -j
+
+
+#------- Program execution -------#
+export MPLCONFIGDIR="/work/gs58/d58004/tmp/matplotlib"
+export WANDB_CONFIG_DIR="/work/gs58/d58004/tmp/wandb"
+export HF_HOME="/work/gs58/d58004/tmp"
+export PATH="/work/02/gs58/d58004/mambaforge/envs/torch/bin/:$PATH"
+nvidia-smi
+cd /work/gs58/d58004/ideas/RibonanzaNet/ribonanzanet2d-final
+echo "Start training"
+python train_ribonanzanet.py --save_dir saved_models_mamaba --epochs 100 --batch_size 32 --lr 0.001 --max_seq_length 68 --use_mamba_end True --config_path configs/pairwise_no_mamba.yaml --use_hybrid True
